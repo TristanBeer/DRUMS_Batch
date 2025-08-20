a5f12afbe169e51d4d9a20825ffaef438860fe75
TristanBeer <tristan99b@hotmail.com>
2025-08-20 15:51:15 -0300

    added additional save paths and sizes

--- batch.py
+++ batch.py
@@ -10,25 +10,39 @@ def get_png_images(folder):
             png_images.append(os.path.join(folder, file))
     return png_images
 
-# Takes a list of image paths and overlays them onto Background1.jpg
+def get_jpg_images(folder):
+    png_images = []
+    for file in os.listdir(folder):
+        if file.lower().endswith('.jpg'):
+            png_images.append(os.path.join(folder, file))
+    return png_images
+
 def compose_png_image(image):
     try:
         img = Image.open(image)
+        img_name = os.path.basename(os.path.splitext(image)[0])
         if img.size[0] != 2500 or img.size[1] != 2000:
             img = img.resize((2000, int((img.size[1] / img.size[0]) * 2000)))
         bg = Image.open(background_image)
         bg.paste(img, (0, 0), img)
-        bg.save(os.path.join(temp_folder, os.path.basename(image)))
+        save_path = os.path.join(temp_folder, img_name + ".jpg")
+        bg.save(save_path, format="JPEG", quality=95)
     except Exception as e:
         print(f"Error processing {image}: {e}")
+    
 
 
 if __name__ == "__main__":
-    start = time.time()
     source_folder = "./MISC_2"
     temp_folder = "./MISC_2/temp"
+    verified_folder, ver_size = "./VERIFIED_IMAGES", (320, 400)
+    verified_large_folder, verl_size = "./VERIFIED_IMAGES_LARGE", (640, 800)
+    thumbnails_folder, thum_size = "./THUMBNAILS", (140, 175)
+    composite_folder, comp_size = "./COMPOSITE_IMAGES", (640, 800)
+
     global background_image
     background_image = "./background1.jpg"
+
 
     if not os.path.isdir(temp_folder):
         os.mkdir(temp_folder)
@@ -38,7 +52,16 @@ if __name__ == "__main__":
     with Pool(processes=12) as pool:
         pool.map(compose_png_image, images)
 
-    end = time.time()
-    print(end - start)
+    for image in get_jpg_images(temp_folder):
+        img = Image.open(image)
+        small_image = img.resize(thum_size)
+        normal_image = img.resize(ver_size)
+        large_image = img.resize(comp_size)
+        
+        small_image.save(os.path.join(thumbnails_folder, os.path.basename(image)), format="JPEG", quality=95)
+        normal_image.save(os.path.join(verified_folder, os.path.basename(image)), format="JPEG", quality=95)
+        large_image.save(os.path.join(verified_large_folder, os.path.basename(image)), format="JPEG", quality=95)
+        large_image.save(os.path.join(composite_folder, os.path.basename(image)), format="JPEG", quality=95)
+
 
 
